32042564c81704f0a5ece3d5dc076f7b8371560a
Vatsal Manot <vatsal.manot@yahoo.com>
2023-11-07 14:57:07 -0800

    Update package

--- Sources/Intramodular/Views/ChatMessageStack.swift
+++ Sources/Intramodular/Views/ChatMessageStack.swift
@@ -29,23 +29,46 @@ public struct ChatMessageStack<Content: View>: View {
     }
     
     private func makeBody(width: CGFloat) -> some View {
-        _VariadicViewAdapter(content) { subviews in
-            LazyVStack(spacing: 0) {
-                _ForEachSubview(
-                    enumerating: subviews,
-                    trait: \._chatItemConfiguration
-                ) { (index, subview, trait) in
-                    if let role = trait.role as? ChatItemRoles.SenderRecipient {
-                        subview
-                            .contentShape(Rectangle())
-                            .frame(maxWidth: min(width * 0.7, 800), alignment: role == .sender ? .trailing : .leading)
-                            .frame(width: .greedy, alignment: role == .sender ? .trailing : .leading)
-                            .id(trait.id)
+        ScrollViewReader { scrollView in
+            _VariadicViewAdapter(content) { subviews in
+                let lastID = subviews.children.last?[trait: \._chatItemConfiguration]?.id
+                
+                LazyVStack(spacing: 0) {
+                    _ForEachSubview(
+                        enumerating: subviews,
+                        trait: \._chatItemConfiguration
+                    ) { (index, subview, trait) in
+                        if let role = trait.role as? ChatItemRoles.SenderRecipient {
+                            subview
+                                .contentShape(Rectangle())
+                                .frame(maxWidth: min(width * 0.7, 800), alignment: role == .sender ? .trailing : .leading)
+                                .frame(width: .greedy, alignment: role == .sender ? .trailing : .leading)
+                                .id(trait.id)
+                        }
                     }
                 }
-            }
-            .onChange(of: subviews.isEmpty) { _ in
-                self._viewID = UUID()
+                .onAppear {
+                    guard let lastID else {
+                        return
+                    }
+                    
+                    withAnimation(after: .milliseconds(200)) {
+                        scrollView.scrollTo(lastID)
+                    }
+                }
+                .onChange(of: subviews.isEmpty) { _ in
+                    self._viewID = UUID()
+                }
+                .withChangePublisher(for: lastID) { lastID in
+                    lastID.compactMap({ $0 })
+                        .removeDuplicates()
+                        .debounce(for: .milliseconds(200), scheduler: DispatchQueue.main)
+                        .sink { id in
+                            withAnimation {
+                                scrollView.scrollTo(id)
+                            }
+                        }
+                }
             }
         }
     }
